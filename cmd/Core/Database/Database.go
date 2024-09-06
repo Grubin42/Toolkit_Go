9ef3cdb4c3a4d30ab844229ede6361bbdb24ef63
gael rubin <concilasplebisll@gmail.com>
2024-09-06 16:44:27 +0200

    fix: db connection

--- cmd/Core/Database/Database.go
+++ cmd/Core/Database/Database.go
@@ -3,11 +3,14 @@ package database
 import (
 	"database/sql"
 	"fmt"
+	"log"
 	"os"
+	"time"
+
 	_ "github.com/go-sql-driver/mysql" // Import du driver MySQL
 )
 
-// ConnectDB établit la connexion à la base de données MySQL
+// ConnectDB établit la connexion à la base de données MySQL avec un mécanisme de retry
 func ConnectDB() (*sql.DB, error) {
 	dbHost := os.Getenv("DATABASE_HOST")
 	dbPort := os.Getenv("DATABASE_PORT")
@@ -19,15 +22,19 @@ func ConnectDB() (*sql.DB, error) {
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true",
 		dbUser, dbPassword, dbHost, dbPort, dbName)
 
-	db, err := sql.Open("mysql", dsn)
-	if err != nil {
-		return nil, err
+	var db *sql.DB
+	var err error
+
+	// Retry mechanism: essayer de se connecter jusqu'à 10 fois, avec une attente de 2 secondes entre chaque tentative
+	for i := 0; i < 10; i++ {
+		db, err = sql.Open("mysql", dsn)
+		if err == nil && db.Ping() == nil {
+			return db, nil
+		}
+
+		log.Printf("Tentative de connexion à MySQL échouée. Réessai dans 2 secondes... (%d/10)\n", i+1)
+		time.Sleep(2 * time.Second)
 	}
 
-	// Vérifier la connexion en effectuant un ping
-	if err := db.Ping(); err != nil {
-		return nil, err
-	}
-
-	return db, nil
+	return nil, fmt.Errorf("impossible de se connecter à MySQL: %v", err)
 }
